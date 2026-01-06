ae89f6499e7e94f8b4540edb78ab45b814f9e45a
BalaRakesh <balarakeshg16@gmail.com>
2026-01-06 20:31:38 +0530

    Fixes

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,9 @@
+// Add message event handler at initial evaluation to prevent warning
+self.addEventListener('message', (event) => {
+  // Handle messages from the page
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
